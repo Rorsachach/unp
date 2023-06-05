9a0386ea6f00ac256d159ebef63ab7c9e2486cf4
Rorsachach <2830999144@qq.com>
2023-06-05 22:22:59 +0800

    update .gitignore

---
+++ build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/1-tcp/client.c.o"
+  "CMakeFiles/client.dir/src/1-tcp/client.c.o.d"
+  "bin/1-TCP/client"
+  "bin/1-TCP/client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/1-tcp/server.c.o"
+  "CMakeFiles/server.dir/src/1-tcp/server.c.o.d"
+  "bin/1-TCP/server"
+  "bin/1-TCP/server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
